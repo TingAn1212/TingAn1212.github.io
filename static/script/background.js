6cf7ae28ad92b25baa5f961585e5d9356e1d3557
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-01-17 00:35:09 +0800

    spawn test 2

--- static/script/background.js
+++ static/script/background.js
@@ -1,7 +1,10 @@
 async function sleep(time){
     await new Promise(r => setTimeout(r, time));
 }
-while (true){
-    await sleep(200);
-    console.log("Spawn")
+async function onStart(){
+    while (true){
+        await sleep(200);
+        console.log("Spawn")
+    }
 }
+onStart();
