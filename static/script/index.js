c96afbe6d3e88cf7dbaeb17f14d7e317bffa980b
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-01-17 22:12:44 +0800

    button function 1

--- static/script/index.js
+++ static/script/index.js
@@ -1,3 +1,15 @@
 $(function(){
     $("#includeBackground").load("background.html"); 
 });
+var open = false;
+function toggle(){
+    if (open){
+        //close
+        open = false;
+    }else{
+        //open
+        open = true;
+        $("bar-v").css("height","0");
+        $("bar-v").css("margin-top","50%");
+    }
+}
