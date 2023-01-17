ef4c6f29d6d9f022d2739e38386c482bb4379867
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-01-17 22:47:40 +0800

    item 4

--- static/script/index.js
+++ static/script/index.js
@@ -9,13 +9,13 @@ function toggle(){
         $("#bar-v").css("height","100%");
         $("#bar-v").css("margin-top","0%");
         $("#bar-h").css("margin-top","-55%");
-        $("#sidebar-content").slideToggle(700);
+        $("#sidebar-content").slideToggle(500);
     }else{
         //open
         open = true;
         $("#bar-v").css("height","0");
         $("#bar-v").css("margin-top","50%");
         $("#bar-h").css("margin-top","-2.5%");
-        $("#sidebar-content").slideToggle(700);
+        $("#sidebar-content").slideToggle(500);
     }
 }
