159e0ff9f830f3f7ca15c86120b92d7d582fedbf
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-01-17 17:08:45 +0800

    rng test

--- static/script/background.js
+++ static/script/background.js
@@ -5,21 +5,26 @@ async function sleep(time){
 function randomCoord(){
     let start = 10;
     let stop = screen.height-42;
-    return String(Math.round(start+(Math.random()*stop)));
+    return String(Math.round(start+(Math.random()*(stop-start))));
+}
+function rng(start,stop){
+    return Math.round(start+(Math,random()*(stop-start)));
 }
 async function onStart(){
     while (true){
         await sleep(100);
-        if (total < 7){
-            var item = $("<div class='spawn'></div>");
-            $(item).css("margin-top",randomCoord()+"px");
-            $(item).css("margin-left","100vw");
-            $(item).animate({'marginLeft':"0vw"},10000,function(){
-                total--;
-                this.remove();
-            });
-            $("#background").append(item);
-            total++;
+        if (rng(1,15) == 2){
+            if (total < 7){
+                var item = $("<div class='spawn'></div>");
+                $(item).css("margin-top",randomCoord()+"px");
+                $(item).css("margin-left","100vw");
+                $(item).animate({'marginLeft':"-20vw"},{duration: 10000, easing: "linear" },function(){
+                    total--;
+                    this.remove();
+                });
+                $("#background").append(item);
+                total++;
+            }
         }
     }
 }
