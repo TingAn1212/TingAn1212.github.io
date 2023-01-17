22df01825b2581472e38c1d98944259f3b886761
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-01-17 22:18:14 +0800

    button function 3

--- static/script/index.js
+++ static/script/index.js
@@ -6,10 +6,14 @@ function toggle(){
     if (open){
         //close
         open = false;
+        $("#bar-v").css("height","100%");
+        $("#bar-v").css("margin-top","0%");
+        $("#bar-h").css("margin-top","-55%");
     }else{
         //open
         open = true;
         $("#bar-v").css("height","0");
         $("#bar-v").css("margin-top","50%");
+        $("#bar-h").css("margin-top","50%");
     }
 }
