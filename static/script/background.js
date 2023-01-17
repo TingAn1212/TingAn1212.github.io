e4036ce9156fb76b967295657ee16d3a57c51240
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-01-17 17:31:29 +0800

    font size randomizer

--- static/script/background.js
+++ static/script/background.js
@@ -27,6 +27,9 @@ async function onStart(){
         if (rng(1,15) == 2 && cd < 0){
             if (total < 5){
                 var item = $("<div class='spawn'>"+randomBin()+"</div>");
+                var size = rng(24,32);
+                $(item).css("height",size+"px");
+                $(item).css("font-size",size+"px");
                 $(item).css("margin-top",randomCoord()+"px");
                 $(item).css("margin-left","100vw");
                 $(item).animate({'marginLeft':"-20vw"},10000, "linear",function(){
