4b7d22f2cd500e494756cfea3bb6227e45092b02
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-01-17 16:20:35 +0800

    random test 1

--- static/script/background.js
+++ static/script/background.js
@@ -1,10 +1,15 @@
 async function sleep(time){
     await new Promise(r => setTimeout(r, time));
 }
+function randomCoord(){
+    let start = 10;
+    let stop = screen.height-42;
+    return start+(Math.random()*stop);
+}
 async function onStart(){
     while (true){
         await sleep(100);
-        console.log("Spawn")
+        console.log(randomCoord);
     }
 }
 onStart();
