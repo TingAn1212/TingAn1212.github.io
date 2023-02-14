68aaacf853544b513e5b14b971c20adc62797c46
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-02-14 09:43:55 +0800

    bugtest 3

--- static/script/my-project.js
+++ static/script/my-project.js
@@ -1,9 +1,9 @@
 function hover(ele){
     var element = $(ele);
-    element.attr("src",element.data("original"));
+    element.attr("src",element.data("target"));
 }
 
 function leave(ele){
     var element = $(ele);
-    element.attr("src",element.data("target"));
+    element.attr("src",element.data("original"));
 }
