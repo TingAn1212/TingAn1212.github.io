ebd795697ae1013d90d8c63257beef857d472eaa
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-02-16 11:03:33 +0800

    size test

--- static/script/my-project.js
+++ static/script/my-project.js
@@ -1,9 +1,15 @@
 function hover(ele){
     var element = $(ele);
     element.attr("src",element.data("target"));
+    element.css("height","auto");
+    element.css("width","100%");
+    $("#B").css("width","100%");
 }
 
 function leave(ele){
     var element = $(ele);
     element.attr("src",element.data("original"));
+    element.css("height","40vh");
+    element.css("width","auto");
+    $("#B").css("width","40vh");
 }
